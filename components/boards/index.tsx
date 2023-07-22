05b0c93e082c8f6437c1bff4bb3f7c0968dafc01
lsj0202 <w1004123@gmail.com>
2023-07-23 02:40:10 +0900

    feat: add 우편번호 검색창

--- components/boards/index.tsx
+++ components/boards/index.tsx
@@ -38,7 +38,12 @@ export const FETCH_BOARD = gql`
     }
   }
 `;
-export default function index(props) {
+
+interface Props {
+  isEdit: boolean;
+}
+
+export default function index(props: Props) {
   interface EBoard {
     boardId: string | number;
     password: string | number;
@@ -56,11 +61,17 @@ export default function index(props) {
   const [password, setPassword] = useState<string | number>("");
   const [title, setTitle] = useState<string>("");
   const [contents, setContents] = useState<string>("");
+  const [isOpen, setIsOpen] = useState(false);
 
   const [writerError, setWriterError] = useState<string>("");
   const [passwordError, setPasswordError] = useState<string>("");
   const [titleError, setTitleError] = useState<string>("");
   const [contentsError, setContentsError] = useState<string>("");
+
+  const [youtubeUrl, setYoutubeUrl] = useState<string>("");
+  const [zipcode, setZipcode] = useState<string>("");
+  const [address, setAddress] = useState<string>("");
+  const [addressDetail, setAddressDetail] = useState<string>("");
 
   useEffect(() => {
     // Fetch된 데이터가 있을 때만 해당 상태 변수들을 업데이트합니다.
@@ -113,6 +124,25 @@ export default function index(props) {
       alert(error.message);
     }
   };
+  const onChangeYoutubeUrl = (e: ChangeEvent<HTMLInputElement>): void => {
+    setYoutubeUrl(e.target.value);
+  };
+
+  const onChangeAddressDetail = (e: ChangeEvent<HTMLInputElement>): void => {
+    setAddressDetail(e.target.value);
+  };
+
+  const onClickAddressSearch = (): void => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const onCompleteAddressSearch = (data: any): void => {
+    // 주소 저장
+    console.log(data);
+    setAddress(data.address);
+    setZipcode(data.zonecode);
+    setIsOpen((prev) => !prev);
+  };
 
   const onClickSubmit = async () => {
     if (!writer) {
@@ -137,6 +167,12 @@ export default function index(props) {
               password,
               title,
               contents,
+              youtubeUrl,
+              boardAddress: {
+                zipcode,
+                address,
+                addressDetail,
+              },
             },
           },
         });
@@ -149,115 +185,146 @@ export default function index(props) {
   };
 
   return (
-    <S.Container>
-      <S.Doc>
-        <S.TitleDiv>게시물 {props.isEdit ? "수정" : "등록"}</S.TitleDiv>
-        <S.WriteDiv>
-          <S.Tag>
-            <S.Text>작성자</S.Text>
-            <S.Inputed
-              type="text"
-              placeholder="이름을 작성해주세요."
-              onChange={onChangeWriter}
-              defaultValue={data?.fetchBoard.writer}
-            />
-            <S.ErrorMessage>{writerError}</S.ErrorMessage>
-          </S.Tag>
-          <S.Tag>
-            <S.Text>비밀번호</S.Text>
-            <S.Inputed
-              type="password"
-              placeholder="비밀번호를 작성해주세요."
-              onChange={onChangePassword}
-              defaultValue={data?.fetchBoard.password}
-            />
-            <S.ErrorMessage>{passwordError}</S.ErrorMessage>
-          </S.Tag>
-        </S.WriteDiv>
-        <S.TextDiv>
-          <S.Tag2>
-            <S.Text>제목</S.Text>
-            <S.Inputed2
-              type="text"
-              placeholder="제목을 작성해주세요."
-              onChange={onChangeTitle}
-              defaultValue={data?.fetchBoard.title}
-            />
-            <S.ErrorMessage>{titleError}</S.ErrorMessage>
-          </S.Tag2>
-        </S.TextDiv>
-        <S.TextDiv2>
-          <S.Tag2>
-            <S.Text>내용</S.Text>
-            <S.Inputed3
-              placeholder="내용을 작성해주세요."
-              onChange={onChangeContents}
-              defaultValue={data?.fetchBoard.contents}
-            />
-            <S.ErrorMessage>{contentsError}</S.ErrorMessage>
-          </S.Tag2>
-        </S.TextDiv2>
-        <S.AddressDiv>
-          <S.Tag2>
-            <S.Text>주소</S.Text>
-            <S.SectionAdd>
-              <S.Inputed5 placeholder="07250" />
-              <S.BtnBlack>우편번호 검색</S.BtnBlack>
-            </S.SectionAdd>
+    <>
+      {isOpen && (
+        <S.AddressModal visible={true}>
+          <S.AddressSearchInput onComplete={onCompleteAddressSearch} />
+        </S.AddressModal>
+      )}
+      <S.Container>
+        <S.Doc>
+          <S.TitleDiv>게시물 {props.isEdit ? "수정" : "등록"}</S.TitleDiv>
+          <S.WriteDiv>
+            <S.Tag>
+              <S.Text>작성자</S.Text>
+              <S.Inputed
+                type="text"
+                placeholder="이름을 작성해주세요."
+                onChange={onChangeWriter}
+                defaultValue={data?.fetchBoard.writer}
+              />
+              <S.ErrorMessage>{writerError}</S.ErrorMessage>
+            </S.Tag>
+            <S.Tag>
+              <S.Text>비밀번호</S.Text>
+              <S.Inputed
+                type="password"
+                placeholder="비밀번호를 작성해주세요."
+                onChange={onChangePassword}
+                defaultValue={data?.fetchBoard.password}
+              />
+              <S.ErrorMessage>{passwordError}</S.ErrorMessage>
+            </S.Tag>
+          </S.WriteDiv>
+          <S.TextDiv>
             <S.Tag2>
-              <S.Inputed2 type="text" />
+              <S.Text>제목</S.Text>
+              <S.Inputed2
+                type="text"
+                placeholder="제목을 작성해주세요."
+                onChange={onChangeTitle}
+                defaultValue={data?.fetchBoard.title}
+              />
+              <S.ErrorMessage>{titleError}</S.ErrorMessage>
             </S.Tag2>
+          </S.TextDiv>
+          <S.TextDiv2>
             <S.Tag2>
-              <S.Inputed2 type="text" />
+              <S.Text>내용</S.Text>
+              <S.Inputed3
+                placeholder="내용을 작성해주세요."
+                onChange={onChangeContents}
+                defaultValue={data?.fetchBoard.contents}
+              />
+              <S.ErrorMessage>{contentsError}</S.ErrorMessage>
             </S.Tag2>
-          </S.Tag2>
-        </S.AddressDiv>
-        <S.TextDiv>
-          <S.Tag2>
-            <S.Text>유튜브</S.Text>
-            <S.Inputed2 type="text" placeholder="링크를 복사해주세요." />
-          </S.Tag2>
-        </S.TextDiv>
-        <S.TextDiv3>
-          <S.Tag2>
-            <S.Text>사진첨부</S.Text>
-            <S.Pictures>
-              <S.Picture>
-                <S.Plus>+</S.Plus>
-                <S.Upload>upload</S.Upload>
-              </S.Picture>
+          </S.TextDiv2>
+          <S.AddressDiv>
+            <S.Tag2>
+              <S.Text>주소</S.Text>
+              <S.SectionAdd>
+                <S.Inputed5
+                  placeholder="07250"
+                  readOnly
+                  value={
+                    zipcode !== ""
+                      ? zipcode
+                      : data?.fetchBoard.boardAddress?.zipcode ?? ""
+                  }
+                />
+                <S.BtnBlack onClick={onClickAddressSearch}>
+                  우편번호 검색
+                </S.BtnBlack>
+              </S.SectionAdd>
+              <S.Tag2>
+                <S.Inputed2
+                  type="text"
+                  readOnly
+                  value={
+                    address !== ""
+                      ? address
+                      : data?.fetchBoard.boardAddress?.address ?? "" // ?? 는 null 또는 undefined일 경우 "" 으로 값을 대체함
+                  }
+                />
+              </S.Tag2>
+              <S.Tag2>
+                <S.Inputed2
+                  type="text"
+                  onChange={onChangeAddressDetail}
+                  defaultValue={
+                    data?.fetchBoard.boardAddress?.addressDetail ?? ""
+                  }
+                />
+              </S.Tag2>
+            </S.Tag2>
+          </S.AddressDiv>
+          <S.TextDiv>
+            <S.Tag2>
+              <S.Text>유튜브</S.Text>
+              <S.Inputed2 type="text" placeholder="링크를 복사해주세요." />
+            </S.Tag2>
+          </S.TextDiv>
+          <S.TextDiv3>
+            <S.Tag2>
+              <S.Text>사진첨부</S.Text>
+              <S.Pictures>
+                <S.Picture>
+                  <S.Plus>+</S.Plus>
+                  <S.Upload>upload</S.Upload>
+                </S.Picture>
 
-              <S.Picture>
-                <S.Plus>+</S.Plus>
-                <S.Upload>upload</S.Upload>
-              </S.Picture>
+                <S.Picture>
+                  <S.Plus>+</S.Plus>
+                  <S.Upload>upload</S.Upload>
+                </S.Picture>
 
-              <S.Picture>
-                <S.Plus>+</S.Plus>
-                <S.Upload>upload</S.Upload>
-              </S.Picture>
-            </S.Pictures>
-          </S.Tag2>
-        </S.TextDiv3>
-        <S.TextDiv3>
-          <S.Tag2>
-            <S.Text>메인설정</S.Text>
-            <S.SectionAdded>
-              <S.Log>
+                <S.Picture>
+                  <S.Plus>+</S.Plus>
+                  <S.Upload>upload</S.Upload>
+                </S.Picture>
+              </S.Pictures>
+            </S.Tag2>
+          </S.TextDiv3>
+          <S.TextDiv3>
+            <S.Tag2>
+              <S.Text>메인설정</S.Text>
+              <S.SectionAdded>
+                <S.Log>
+                  <S.Inputs type="radio" name="btn" />
+                  유튜브
+                </S.Log>
                 <S.Inputs type="radio" name="btn" />
-                유튜브
-              </S.Log>
-              <S.Inputs type="radio" name="btn" />
-              사진
-            </S.SectionAdded>
-          </S.Tag2>
-        </S.TextDiv3>
-        <S.TextDiv2>
-          <S.Complete onClick={props.isEdit ? onClickEdit : onClickSubmit}>
-            {props.isEdit ? "수정" : "등록"}하기
-          </S.Complete>
-        </S.TextDiv2>
-      </S.Doc>
-    </S.Container>
+                사진
+              </S.SectionAdded>
+            </S.Tag2>
+          </S.TextDiv3>
+          <S.TextDiv2>
+            <S.Complete onClick={props.isEdit ? onClickEdit : onClickSubmit}>
+              {props.isEdit ? "수정" : "등록"}하기
+            </S.Complete>
+          </S.TextDiv2>
+        </S.Doc>
+      </S.Container>
+    </>
   );
 }
--- components/boards/style.ts
+++ components/boards/style.ts
@@ -1,4 +1,6 @@
-import styled from '@emotion/styled'
+import styled from "@emotion/styled";
+import { Modal } from "antd";
+import DaumPostcode from "react-daum-postcode"; // 우편 번호
 
 export const Container = styled.div`
   display: flex;
@@ -6,12 +8,12 @@ export const Container = styled.div`
   justify-content: center;
   width: 100%;
   height: 200vh;
-`
+`;
 export const Doc = styled.div`
   width: 60%;
   height: 180vh;
   box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-`
+`;
 export const TitleDiv = styled.div`
   width: 100%;
   height: 20vh;
@@ -20,75 +22,75 @@ export const TitleDiv = styled.div`
   justify-content: center;
   font-size: 32px;
   font-weight: 400;
-`
+`;
 export const WriteDiv = styled.div`
   width: 100%;
   height: 10vh;
   display: flex;
   align-items: center;
   justify-content: space-evenly;
-`
+`;
 export const Inputed = styled.input`
   width: 24vw;
   height: 40px;
-  border-radius: #BDBDBD;
+  border-radius: #bdbdbd;
   padding: 10px;
   margin-top: 10px;
-`
+`;
 export const Tag = styled.div`
   width: 40%;
   height: 80px;
-`
+`;
 export const Text = styled.span`
   color: black;
-`
+`;
 export const TextDiv = styled.div`
   width: 100%;
   height: 20vh;
   display: flex;
   align-items: center;
   justify-content: center;
-`
+`;
 export const Inputed2 = styled.input`
   margin-top: 10px;
   width: 51.5vw;
   height: 40px;
   padding: 10px;
-`
+`;
 export const Tag2 = styled.div`
   width: 86%;
   height: 80px;
   display: flex;
   flex-direction: column;
-`
+`;
 export const TextDiv2 = styled.div`
   width: 100%;
-  height:38vh;
+  height: 38vh;
   display: flex;
   justify-content: center;
-`
+`;
 export const Inputed3 = styled.textarea`
   margin-top: 10px;
-  padding : 10px;
+  padding: 10px;
   width: 51.5vw;
   height: 100px;
   resize: none;
   padding: 10px 0px 200px 10px;
   overflow-y: hidden;
-`
+`;
 export const AddressDiv = styled.div`
   width: 100%;
   height: 25vh;
   display: flex;
   justify-content: center;
-`
+`;
 export const Inputed4 = styled.input`
   width: 5vw;
   padding-left: 15px;
   height: 5vh;
   margin-top: 10px;
   overflow-y: hidden;
-`
+`;
 export const SectionAdd = styled.div`
   margin-top: 10px;
   width: 29vh;
@@ -96,12 +98,12 @@ export const SectionAdd = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-`
+`;
 export const Inputed5 = styled.input`
   width: 75px;
   height: 5vh;
   padding-left: 15px;
-`
+`;
 export const BtnBlack = styled.button`
   width: 110px;
   height: 5vh;
@@ -110,20 +112,20 @@ export const BtnBlack = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 export const TextDiv3 = styled.div`
   width: 100%;
   height: 18vh;
   display: flex;
   justify-content: center;
-`
+`;
 export const Pictures = styled.div`
   width: 50%;
   height: 100px;
   margin-top: 10px;
   display: flex;
   flex-direction: row;
-`
+`;
 export const Picture = styled.div`
   width: 60px;
   height: 60px;
@@ -133,44 +135,54 @@ export const Picture = styled.div`
   flex-direction: column;
   background-color: gray;
   margin: 0 20px 0 0;
-`
+`;
 export const Plus = styled.div`
   font-size: 25px;
   color: black;
-`
+`;
 export const Upload = styled.div`
   font-size: 15px;
   color: black;
-`
+`;
 export const Inputs = styled.input`
- &:hover{
-  cursor: pointer;
+  &:hover {
+    cursor: pointer;
   }
-  accent-color: #FFD600;
-`
+  accent-color: #ffd600;
+`;
 export const SectionAdded = styled.div`
   width: 18vw;
   height: 10vh;
   display: flex;
   align-items: center;
-`
+`;
 export const Log = styled.div`
   width: 6vw;
-`
+`;
 export const Complete = styled.button`
   width: 10vw;
   height: 5vh;
   border: none;
-  background-color: #FFD600;
-  &:hover{
-  cursor: pointer;
+  background-color: #ffd600;
+  &:hover {
+    cursor: pointer;
   }
-  &:active{
+  &:active {
     background-color: orange;
   }
-`
+`;
 export const ErrorMessage = styled.span`
   color: red;
   font-size: 12px;
   margin-top: 5px;
-`
+`;
+
+export const Error = styled.div`
+  padding-top: 10px;
+  font-size: 14px;
+  color: red;
+`;
+
+export const AddressModal = styled(Modal)``; // 라이브러리 이모션으로 감싸기
+
+export const AddressSearchInput = styled(DaumPostcode)``; // 라이브러리 이모션으로 감싸기
